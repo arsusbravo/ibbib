27b805ba1c8e9c2d90354fe46737cd156e2f882e
ayoksus <ario_susmaryono@hotmail.com>
2020-02-09 23:08:34 +0100

    client nav

--- resources/views/client/includes/nav.blade.php
+++ resources/views/client/includes/nav.blade.php
@@ -2,7 +2,7 @@
     <ul class="nav navbar-nav">
         <li {!! \Request::path() == '/' ? 'class="active"': '' !!}><a href="{{ url('/') }}">Home</a></li>
         <li {!! \Request::path() == 'client' ? 'class="active"': '' !!}><a href="{{ url('client') }}">{{ __('Post A Project for free') }}</a></li>
-        <li {!! \Request::path() == 'translators' ? 'class="active"': '' !!}><a href="{{ url('translators') }}">{{ __('Translator List') }}</a></li>
+        <li {!! \Request::path() == 'client/translators' ? 'class="active"': '' !!}><a href="{{ url('client/translators') }}">{{ __('Translator List') }}</a></li>
         <li {!! \Request::path() == 'contact' ? 'class="active"': '' !!}><a href="{{ url('contact') }}">{{ __('Contact') }}</a></li>
     </ul>
 </div>
@@ -10,7 +10,7 @@
 <div class="navbar-right">
     <ul class="sign-in tr-list">
         <li><i class="fa fa-user"></i></li>
-        <li><a href="{{ url('account') }}">{{ __('Account') }}</a></li>
+        <li><a href="{{ url('client/account') }}">{{ __('Account') }}</a></li>
         <li>
             <a href="{{ route('logout') }}"
                 onclick="event.preventDefault();
